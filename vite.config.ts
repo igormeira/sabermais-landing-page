f6550fbd4962e372ff04c4457c857d28556a6430
Igor Meira <igor.meira@gmail.com>
2025-01-13 08:16:27 -0300

    Add CNAME

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/sabermais-landing-page/',
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  plugins: [react()]
 });

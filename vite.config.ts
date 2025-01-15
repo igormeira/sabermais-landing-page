82224a948eae7aac4ad507550d81e5c59a412e90
Igor Meira <igormeira.dev@gmail.com>
2025-01-15 17:16:28 -0300

    Add docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/sabermais-landing-page/',
+  build: {
+    outDir: 'docs', // Output files to the /docs folder
+  },
   plugins: [react()]
 });
